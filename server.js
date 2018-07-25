59e9bbd54bee4bd0a193281ffbe4eac841e4d3bf
Lu <luspace6@163.com>
2018-07-25 20:42:34 +0800

    feature: hello-world demo

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {
+        "Content-type": "text-plain"
+    });
+    response.write("Hello World!");
+    response.end();
+}).listen(8000);
