ddd1979b6b11d5e12deca3e2ec5689c293942053
Lu <luspace6@163.com>
2018-07-25 20:57:45 +0800

    feature: 模块化

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
-http.createServer((request, response) => {
-    response.writeHead(200, {
-        "Content-type": "text-plain"
-    });
-    response.write("Hello World!");
-    response.end();
-}).listen(8000);
+const start = () => {
+    http.createServer((request, response) => {
+        console.log('request received');
+        response.writeHead(200, {
+            "Content-type": "text-plain"
+        });
+        response.write("Hello World!");
+        response.end();
+    }).listen(8000);
+
+    console.log('Server has started');
+};
+
+exports.start = start;
